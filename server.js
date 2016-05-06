f10edb81aa337cc31c13785dc51ab578e9af61a2
Terrence <thenriquez25@outlook.com>
2016-05-05 21:03:21 -0400

    well changes was made

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ app.use ("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
 app.use ("/img", express.static("public/img"));
 
-app.get("/" , function(req, res){
-    res.sendFile(process.cwd() + "views/index.html");
+
+app.get("#" , function(req, res){
+    res.sendFile(process.cwd() + "/views/home.html");
 });
 app.get("/contact" , function(req, res){
-  res.sendFile(process.cwd() + "views/contact.html");
-});
-app.get("/profile" , function(req, res){
-  res.sendFile(process.cwd() + "views/profile.html");
-});
-app.get("/portfolio" , function(req, res){
-  res.sendFile(process.cwd() + "views/portfolio.html");
+  res.sendFile(process.cwd() + "/views/contact.html");
 });
 
+app.get("/profile", function(){
+  res.sendFile(process.cwd() + "/views/profile.html");
 
-
+});
+app.get("/portfilo", function(){
+  res.sendFile(process.cwd() + "/views/portfilo.html");
+});
 
 app.listen(PORT, function(){
   console.log("Listening on port %s", PORT);
