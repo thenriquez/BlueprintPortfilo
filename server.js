c14ca158dcc85a286a73faa44c099e233613c571
Terrence <thenriquez25@outlook.com>
2016-05-13 17:29:16 -0400

    adding some angular

--- server.js
+++ server.js
@@ -4,16 +4,28 @@ var PORT = process.env.PORT || 8080;
 
 
 
+
 app.use ("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
-app.use ("/img", express.static("public/img"));
 
-
-app.get("/home" , function(req, res){
+app.get("*" , function(req, res){
     res.sendFile(process.cwd() + "/views/home.html");
+});
+app.get("/contact" , function(req, res){
+  res.sendFile(process.cwd() + "/views/contact.html");
+});
+
+app.get("/profile", function(){
+  res.sendFile(process.cwd() + "/views/profile.html");
+
+});
+
+app.get("/portfolio", function(){
+  res.sendFile(process.cwd() + "/views/portfolio.html");
+
 });
 
 app.listen(PORT, function(){
   console.log("Listening on port %s", PORT);
 
-});
+})
