6ada920a09be5ce5b123e0f31179bc1b35239eb6
Terrence <thenriquez25@outlook.com>
2016-05-14 00:24:36 -0400

    created an new server.js and routing solutution

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-
-
+var express = require("express");
+var app     = express();
+var path    = require("path");
 
 app.use ("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
 
-app.get("*" , function(req, res){
-    res.sendFile(process.cwd() + "/views/home.html");
-});
-app.get("/contact" , function(req, res){
-  res.sendFile(process.cwd() + "/views/contact.html");
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
-app.get("/profile", function(){
-  res.sendFile(process.cwd() + "/views/profile.html");
-
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
 });
 
-app.get("/portfolio", function(){
-  res.sendFile(process.cwd() + "/views/portfolio.html");
-
+app.get('/portfolio',function(req,res){
+  res.sendFile(path.join(__dirname+'/portfolio.html'));
+});
+app.get('/contact',function(req,res){
+  res.sendFile(path.join(__dirname+'/contact.html'));
 });
 
-app.listen(PORT, function(){
-  console.log("Listening on port %s", PORT);
 
-})
+app.listen(3000);
+
+console.log("Running at Port 3000");
